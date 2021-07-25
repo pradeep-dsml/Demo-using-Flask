75962d042881b6d8f5e0e234ccc3b82db5fddc97
pradeep_yadav <pradeep92.dsml@gmail.com>
2021-07-25 20:33:11 +0530

    second commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask("__name__")
 
 @app.route("/")
 def index():
-    return "flask app is working(github version)ie through github repo"
+    return "<h1>flask app is working(github version)ie through github repo</h1>"
 
 if __name__ == "__main__":
     app.run()
